a053cf3d89a269778aa70d9da69682be8a6641c1
jkya11 <jkya11@kaist.ac.kr>
2014-02-10 15:58:47 +0900

    Create PrintRsp.h

---
+++ PrintRsp.h
@@ -0,0 +1,27 @@
+/*
+ * PrintRsp.h
+ *
+ *  Created on: Feb 6, 2014
+ *      Author: bjchoe
+ */
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+void print_rsp(int count);
+
+void print_rsp(int count) {
+	if(count>=6){
+		cout << "paper" << endl;
+	}
+	else if(count == 4 || count == 5) {
+		cout << "scissors" << endl;
+	}
+	else {
+		cout << "rock" << endl;
+	}
+}
